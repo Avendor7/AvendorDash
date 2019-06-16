8b423a9c5da329b21ed6bceb6843570851075e35
Stephen <steve.wiggins7@gmail.com>
2019-06-15 22:30:11 -0400

    add in the background image and blur

---
+++ client/src/components/BackgroundImage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bg-image"></div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bg-image {
+  /* The image used */
+  background-image: url("./../assets/rpi.jpg");
+
+  /* Add the blur effect */
+  filter: blur(12px);
+  -webkit-filter: blur(12px);
+  transform: scale(1.1);
+
+  /* Full height */
+  height: 100%; 
+  width: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position:fixed;
+}
+</style>
