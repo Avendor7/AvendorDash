61bab4967ed9120bb368072f42b4979d6342ef22
Stephen <steve.wiggins7@gmail.com>
2019-06-26 20:32:10 -0400

    add a bunch of CSS that probably doesn't all need to be there

--- client/src/components/Bookmarks.vue
+++ client/src/components/Bookmarks.vue
@@ -28,5 +28,57 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  /* Position text in the middle of the page/image */
+.custom-card {
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+  color: white;
+  font-weight: bold;
+  border: 3px solid #f1f1f1;
+  position: relative;
+  text-align: center;
+}
 
+
+a {
+  background-color: white;
+  box-shadow: 5px 5px 5px black;
+  color: black;
+  padding: .5em .5em;
+  margin: 1em .5em;
+  position: relative;
+  text-decoration: none;
+  text-transform: uppercase;
+  display: inline-block;
+}
+a span{
+  padding:1em;
+}
+a:hover {
+background-color: grey;
+}
+
+a:active {
+box-shadow: none;
+top: 5px;
+}
+ul{
+  list-style-type: none;
+  padding:0;
+}
+li {
+  display: inline;
+}
+a img{
+  vertical-align: middle;
+  height:100%;
+  width: 25px;
+  
+}
+.searchbox{
+  margin:20px;
+}
+.row{
+  padding-bottom:20px;
+}
 </style>
